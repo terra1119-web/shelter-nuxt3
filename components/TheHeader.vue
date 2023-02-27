cbd4d3980acc695cdc5606f7497d931aced169e2
terra1119-web <terra1119@gmail.com>
2023-02-27 23:18:24 +0900

    - スクロール状態でバッグ

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -8,6 +8,7 @@
 				:to="`/`"
 				class="block w-[77px] h-[20px] bg-[url('/images/logo.svg')]"
 			/>
+			{{ isScrollDown }}
 		</h1>
 		<nav class="hidden md:block">
 			<ul class="flex gap-10 text-xl">
