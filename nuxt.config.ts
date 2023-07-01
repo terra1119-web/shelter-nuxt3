01c47bc3c4a92253a2db413d525e598dce6a3e66
terra1119-web <terra1119@gmail.com>
2023-07-01 16:04:58 +0900

    ssr: trueに戻す

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	ssr: false,
+	ssr: true,
 	runtimeConfig: {
 		public: {
 			apiBase: process.env.NUXT_WP_API_URL,
