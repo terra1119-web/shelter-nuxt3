58036da6b3923e85ca7a23d35cd4a992ca4ccd3e
terra1119-web <terra1119@gmail.com>
2023-07-22 18:25:58 +0900

    - リンク修正

--- composables/useSinglePost.ts
+++ composables/useSinglePost.ts
@@ -12,7 +12,7 @@ export const useSinglePost = async (
 	const date: number = dayjs(paramsDateString).date()
 	const dateString: string = dayjs(`${year}/${month}/${date}`).toISOString()
 
-	const { data } = await useFetch<any>(`/${postType}`, {
+	const { data, refresh } = await useFetch<any>(`/${postType}`, {
 		baseURL: apiBase,
 		key: `${apiBase}/${postType}/${dateString}`,
 		params: {
@@ -23,8 +23,9 @@ export const useSinglePost = async (
 		},
 	})
 
-	// watchEffect(() => {
-	// 	refresh()
-	// })
+	watchEffect(() => {
+		if (!data.value) refresh()
+	})
+
 	return data
 }
